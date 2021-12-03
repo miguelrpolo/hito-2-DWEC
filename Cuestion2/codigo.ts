87464d61e33c96de76b23b82954de2f74ce8b32b
Miguel <55658607+miguelrpolo@users.noreply.github.com>
2021-12-03 18:27:09 +0100

    Add files via upload

--- Cuestion2/codigo.ts
+++ Cuestion2/codigo.ts
@@ -1,10 +1,12 @@
 class Animal{
 
+//Variables en privado  para evitar breachas en ell encapsulamiento
    private nombreAnimal:string;
    private numeroPatas:Number;
    private carnivoro:boolean;
    private edad:number;
 
+   //El contstructor para poder declarar la clase. 
     constructor(nombre:string,patas:number,carne:boolean,eda:number){
         this.nombreAnimal=nombre;
         this.numeroPatas=patas;
@@ -12,6 +14,7 @@ class Animal{
         this.edad=eda;
     }
 
+    //Getters y setters
     public getNombreAnimal(){
         return this.nombreAnimal
     }
@@ -37,10 +40,13 @@ class Animal{
         this.edad=nuevo;
     }
 
+    //Una función toString para identificar bien con qué estamos trabajando
     public toString(){
 return (this.getNombreAnimal() + ", es carnívoro? " + this.getCarnivoro() + ", tiene " +this.getNumeroPatas() + " patas, y además tiene " + this.getEdad() + " años.");
     }
 }
+
+//Se llama a esta función desde el HTML, se crea el array y se llama a la segunda función
 function mostrar(caja:HTMLTextAreaElement){
 let animal1 = new Animal("Conejo",4,false,8);
 let animal2 = new Animal("León",4,true,12);
@@ -53,6 +59,7 @@ lista.push(animal3);
 mostrar2(caja,lista);
 }
 
+//Esta función escribe el animal
 function mostrar2(caja:HTMLTextAreaElement, lista:Array<Animal>){
 lista.forEach(animal=>{
 caja.innerHTML+=(animal.toString() + '\r\n\r\n');
